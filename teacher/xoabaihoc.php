7e94888bfed7dd727fdf745434446dd209d81fe5
phamkyson <phamson2z35@gmail.com>
2026-04-10 13:21:12 +0700

    phamkyson

--- teacher/xoabaihoc.php
+++
@@ -1,9 +0,0 @@
-<h1>Xoa</h1>
-<?php
-include('../connect.php');
-
-$id = $_GET['id'];
-$sql = "DELETE FROM `bai_hoc` WHERE id = '$id'";
-mysqli_query($conn, $sql);
-header('location: index.php?page_layout=baihoc');
-?>
